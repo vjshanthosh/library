0d3d6d441f343dc528115feceb416366f74b8c26
vjshanthosh <102887202+vjshanthosh@users.noreply.github.com>
2025-01-22 18:05:58 +0530

    file updated

--- library.cpp
+++ library.cpp
@@ -4,23 +4,26 @@
 #include <algorithm>
 using namespace std;
 
-void add_user (map<string, string>& users, map<string, vector<string> >& user_book) {
+void add_user (map<int, pair<string, string> >& users, map<int, vector<string> >& user_book) {
+    int id;
     string name, pass;
+    cout << "Enter ID (natural number): ";
+    cin >> id;
     cout << "Enter username: ";
     cin >> name;
     cout << "Enter password: ";
     cin >> pass;
-    users[name] = pass;
-    user_book[name] = {};
+    users[id] = {name, pass};
+    user_book[id] = {};
 }
 
-void remove_user(map<string, string>& users, map<string, vector<string> >& user_book) {
-    string name;
-    cout << "User to remove: ";
-    cin >> name;
-    if (users.count(name)) {
-        users.erase(name);
-        user_book.erase(name);
+void remove_user(map<int, pair<string, string> >& users, map<int, vector<string> >& user_book) {
+    int id;
+    cout << "User ID to remove: ";
+    cin >> id;
+    if (users.count(id)) {
+        users.erase(id);
+        user_book.erase(id);
     }
 }
 
@@ -52,21 +55,22 @@ void remove_book (map<string, int>& books) {
     }
 }
 
-void login(string& current, map<string, string> users) {
-    string name, pass;
-    cout << "Enter username: ";
-    cin >> name;
+void login(int& current, map<int, pair<string, string> > users) {
+    int id;
+    string pass;
+    cout << "Enter ID: ";
+    cin >> id;
     cout << "Enter password: ";
     cin >> pass;
-    if (users.count(name) && users[name] == pass) {
-        current = name;
+    if (users.count(id) && users[id].second == pass) {
+        current = id;
         cout << "Login successful!\n";
     } else {
         cout << "Invalid login.\n";
     }
 }
 
-void get_book (string current, map<string, vector<string> >& user_book, map<string, int>& books) {
+void get_book (int current, map<int, vector<string> >& user_book, map<string, int>& books) {
     string book;
     cout << "What book would you like: ";
     cin >> book;
@@ -88,17 +92,36 @@ void see_books (map<string, int> books) {
     }
 }
 
-void see_users (map<string, string> users, map<string, vector<string> > user_book) {
+void see_users (map<int, pair<string, string> > users) {
+    for (auto it : users) {
+        cout << it.second.first << endl;
+    }
+}
+
+void user_info (map<int, pair<string, string> > users, map<int, vector<string> > user_book) {
     for (auto user : users) {
-        cout << "Username: " << user.first << " Password: " << user.second << " Books: ";
+        cout << "ID: "  << user.first << "\nUsername: " << user.second.first << "\nPassword: " << user.second.second << "\nBooks:\n";
         for (auto book : user_book[user.first]) {
-            cout << book;
+            cout << book << "\n";
         }
         cout << endl;
     }
 }
 
-void return_book (string current, map<string, vector<string> >& user_book, map<string, int>& books) {
+void set_default (map<int, pair<string, string> >& users, map<string, int>& books, map<int, vector<string> >& user_book) {
+    users[1] = {"John", "John#25"};
+    users[2] = {"Sam", "Sam&42"};
+    users[3] = {"George", "George$77"};
+    books["Magic"] = 10;
+    books["Harry Potter"] = 10;
+    books["Quantum Physics"] = 10;
+    books["C++"] = 10;
+    user_book[1] = {"Magic", "Harry Potter"};
+    user_book[2] = {};
+    user_book[3] = {"Quantum Physics", "C++"};
+}
+
+void return_book (int current, map<int, vector<string> >& user_book, map<string, int>& books) {
     string book;
     cout << "What book to return: ";
     cin >> book;
@@ -114,13 +137,14 @@ void return_book (string current, map<string, vector<string> >& user_book, map<s
 }
 
 int main() {
-    map<string, string> users;
-    users["Admin"] = "Admin@123";
+    map<int, pair<string, string> > users;
+    users[0] = {0, "Admin@123"};
     map<string, int> books;
-    map<string, vector<string> > user_book;
-    string current = "";
+    map<int, vector<string> > user_book;
+    set_default(users, books, user_book);
+    int current = -1;
     while (true) {
-        if (current == "") {
+        if (current == -1) {
             string input;
             cout << "Type l to login or q to quit: ";
             cin >> input;
@@ -129,9 +153,9 @@ int main() {
             } else if (input == "q") {
                 break;
             }
-        } else if (current == "Admin") {
+        } else if (current == 0) {
             string input;
-            cout << "Type ab to add book, rb to remove book, au to add user, ru to remove user, sb to see books, su to see users, l to logout, or q to quit: ";
+            cout << "Type ab to add book\n rb to remove book\n au to add user\n ru to remove user\n sb to see books\n su to see users\n ui to see user info\n l to logout\n or q to quit: ";
             cin >> input;
             if (input == "ab") {
                 add_book(books);
@@ -144,15 +168,17 @@ int main() {
             } else if (input == "sb") {
                 see_books(books);
             } else if (input == "su") {
-                see_users(users, user_book);
+                see_users(users);
+            } else if (input == "ui") {
+                user_info(users, user_book);
             } else if (input == "l") {
-                current = "";
+                current = -1;
             } else if (input == "q") {
                 break;
             }
         } else {
             string input;
-            cout << "Type g to get book, r to return book, s to see books, l to logout, or q to quit: ";
+            cout << "Type g to get book\n r to return book\n s to see books\n l to logout\n or q to quit: ";
             cin >> input;
             if (input == "g") {
                 get_book(current, user_book, books);
@@ -161,11 +187,12 @@ int main() {
             } else if (input == "s") {
                 see_books(books);
             } else if (input == "l") {
-                current = "";
+                current = -1;
             } else if (input == "q") {
                 break;
             }
         }
+        cout << endl;
     }
     return 0;
 }
